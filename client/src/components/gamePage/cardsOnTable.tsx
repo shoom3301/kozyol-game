d42213b03eb82d40c17fcf6afe5c7aa80ecc0a28
a.e.kazachenko <a.e.kazachenko@tinkoff.ru>
2020-05-05 14:23:52 +0300

    Fix CardsOnTable

--- client/src/components/gamePage/cardsOnTable.tsx
+++ client/src/components/gamePage/cardsOnTable.tsx
@@ -12,6 +12,26 @@ export class CardsOnTable extends Component<CardsOnTableProps, any> {
   render(): React.ReactElement {
     const slots: Cards[] = []
 
+    if (this.props.cards.length > 0) {
+      const first = this.props.cards[0]
+      const firstUserId = parseInt(Object.keys(first)[0])
+      const firstCards = first[firstUserId]
+      const slotsCount = firstCards.length
+
+      for (let i = 0; i < slotsCount; i++) {
+        this.props.cards.forEach(item => {
+          const userId = parseInt(Object.keys(item)[0])
+          const cards = item[userId]
+
+          slots[i] = slots[i] || []
+
+          if (cards[i]) {
+            slots[i].push(cards[i])
+          }
+        })
+      }
+    }
+
     return (
       <Container>
         <Title>Карты на столе:</Title>
