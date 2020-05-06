30beafde810aabe5bb064d92088c0c081242a17a
a.e.kazachenko <a.e.kazachenko@tinkoff.ru>
2020-05-06 19:55:58 +0300

    Render tricks

--- client/src/components/gamePage/playersList.tsx
+++ client/src/components/gamePage/playersList.tsx
@@ -25,7 +25,8 @@ export class PlayersList extends Component<CardsOnTableProps, any> {
               current={this.props.currentPlayerId === id}
               key={id}>
             <PlayerAvatar src={'https://cdn.iconscout.com/icon/free/png-512/avatar-380-456332.png'}/>
-            <PlayerName>{name}: {this.props.score[id]}</PlayerName>
+              <PlayerName>{name}</PlayerName>
+              <ScoreContainer><PlayerScore>{this.props.score[id]}</PlayerScore></ScoreContainer>
           </PlayersListItem>)}
         </PlayersListContainer>
       </Container>
@@ -43,6 +44,24 @@ export const PlayerAvatar = styled.img`
 `;
 
 export const PlayerName = styled.p`
+  text-align: center;
+  overflow: hidden;
+  margin: 0;
+`;
+
+export const ScoreContainer = styled.p`
+  margin: 0;
+`;
+
+export const PlayerScore = styled.div`
+  display: inline-block;
+  width: 25px;
+  padding: 5px 1px;
+  font-size: 13px;
+  border-radius: 50%;
+  background: rgba(255,193,35,0.83);
+  border: 2px solid #fff;
+  color: #000;
   text-align: center;
 `;
 
---
+++ client/src/components/gamePage/setResults.tsx
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import { Title } from 'ui-elements/form';
+import { cardImage } from 'helpers/cardImage';
+import { Cards, Desk, suitSymbols } from 'model/Card';
+import { CardItem, CardsList, CardSlot, Container } from './elements';
+import { Player } from 'model/Player';
+import styled from 'styled-components';
+
+export interface SetResultsProps {
+  tricks: {
+    [playerId: number]: number
+  }
+  players: Player[]
+}
+
+export class SetResults extends Component<SetResultsProps, any> {
+  render(): React.ReactElement {
+    return (
+      <Container>
+        <Title>Итоги круга:</Title>
+        <ResultsList>
+          {this.props.players.map(player => {
+            return <ResultsListItem><strong>{player.name}</strong>: {this.props.tricks[player.id]}</ResultsListItem>
+          })}
+        </ResultsList>
+      </Container>
+    )
+  }
+}
+
+export const ResultsList = styled.ul`
+  border: 1px solid #000;
+  max-width: 500px;
+  margin: 20px auto;
+  padding: 0;
+  list-style: none;
+`;
+
+export const ResultsListItem = styled.li`
+  border-bottom: 1px solid #000;
+  padding: 10px;
+  
+  :last-child {
+    border-bottom: 0;
+  }
+`;
