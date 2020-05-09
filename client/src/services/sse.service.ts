44963659f6ef4f0f685ad109c56d71ecbeaeea18
a.e.kazachenko <a.e.kazachenko@tinkoff.ru>
2020-05-09 20:47:16 +0300

    SSE implementation

--- client/src/services/sse.service.ts
+++ client/src/services/sse.service.ts
@@ -5,58 +5,93 @@ import { getGameIdByLocation } from 'store/selectors/games'
 import { IState } from 'store/states'
 import { gameStateUpdate } from 'store/actions/gameState'
 import { gameStateService } from 'services/gameState.service'
+import { GameItem } from 'model/GameItem'
+import { GameState } from 'model/GameState'
 
 const pollingTimeout = 2000
 
 export class SseService {
   private gamesTimer: number | null = null
   private gameTimer: number | null = null
+  private sse: EventSource | null = null
 
   connect() {
-    // TODO
+    this.sse = new EventSource('/api/subscribe')
   }
 
   disconnect() {
-    // TODO
+    if (this.sse) this.sse.close()
   }
 
   subscribeToGamesList() {
-    const update = () => {
-      gamesService.getList().then((games) => {
-        store.dispatch(gameFetchAllSuccess(games))
-      })
+    if (this.sse) {
+      this.sse.addEventListener('list', this.onList)
     }
-
-    update()
-
-    this.gamesTimer = setInterval(update, pollingTimeout)
   }
 
   unsubscribeFromGamesList() {
-    if (this.gamesTimer !== null) clearInterval(this.gamesTimer)
+    if (this.sse) {
+      this.sse.removeEventListener('list', this.onList)
+    }
   }
 
   subscribeToGame() {
-    const gameId = parseInt(getGameIdByLocation(store.getState() as IState))
-
-    if (!gameId || isNaN(gameId)) {
-      return
+    if (this.sse) {
+      this.sse.addEventListener('list', this.onState)
     }
-
-    const update = () => {
-      gameStateService.getGameState(gameId).then((state) => {
-        store.dispatch(gameStateUpdate(state))
-      })
-    }
-
-    update()
-
-    this.gameTimer = setInterval(update, pollingTimeout)
   }
 
   unsubscribeFromGame() {
-    if (this.gameTimer !== null) clearInterval(this.gameTimer)
+    if (this.sse) {
+      this.sse.removeEventListener('list', this.onState)
+    }
   }
+
+  onList = (event: any) => {
+    store.dispatch(gameFetchAllSuccess(event.data as GameItem[]))
+  }
+
+  onState = (event: any) => {
+    store.dispatch(gameStateUpdate(event.data as GameState))
+  }
+
+  // subscribeToGamesList() {
+  //   const update = () => {
+  //     gamesService.getList().then((games) => {
+  //       store.dispatch(gameFetchAllSuccess(games))
+  //     })
+  //   }
+  //
+  //   update()
+  //
+  //   this.gamesTimer = setInterval(update, pollingTimeout)
+  // }
+  //
+  // unsubscribeFromGamesList() {
+  //   if (this.gamesTimer !== null) clearInterval(this.gamesTimer)
+  // }
+  //
+  // subscribeToGame() {
+  //   const gameId = parseInt(getGameIdByLocation(store.getState() as IState))
+  //
+  //   if (!gameId || isNaN(gameId)) {
+  //     return
+  //   }
+  //
+  //   const update = () => {
+  //     gameStateService.getGameState(gameId).then((state) => {
+  //       store.dispatch(gameStateUpdate(state))
+  //     })
+  //   }
+  //
+  //   update()
+  //
+  //   this.gameTimer = setInterval(update, pollingTimeout)
+  // }
+  //
+  // unsubscribeFromGame() {
+  //   if (this.gameTimer !== null) clearInterval(this.gameTimer)
+  // }
 }
 
 export const sseService = new SseService()
