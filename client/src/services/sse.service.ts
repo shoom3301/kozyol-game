8c62076e3b7fb227405bcfa46ae9efab24f35bda
a.e.kazachenko <a.e.kazachenko@tinkoff.ru>
2020-05-10 20:18:57 +0300

    Remove polling

--- client/src/services/sse.service.ts
+++ client/src/services/sse.service.ts
@@ -1,19 +1,13 @@
 import { store } from 'store'
 import { gameFetchAllSuccess } from 'store/actions/games'
-import { gamesService } from 'services/games.service'
 import { getGameIdByLocation } from 'store/selectors/games'
 import { IState } from 'store/states'
 import { gameStateUpdate } from 'store/actions/gameState'
-import { gameStateService } from 'services/gameState.service'
 import { GameItem } from 'model/GameItem'
 import { GameState } from 'model/GameState'
 import { authService } from 'services/auth.service'
 
-const pollingTimeout = 2000
-
 export class SseService {
-  private gamesTimer: number | null = null
-  private gameTimer: number | null = null
   private sse: EventSource | null = null
 
   connect() {
@@ -69,44 +63,6 @@ export class SseService {
 
     store.dispatch(gameStateUpdate(state))
   }
-
-  // subscribeToGamesList() {
-  //   const update = () => {
-  //     gamesService.getList().then((games) => {
-  //       store.dispatch(gameFetchAllSuccess(games))
-  //     })
-  //   }
-  //
-  //   update()
-  //
-  //   this.gamesTimer = setInterval(update, pollingTimeout)
-  // }
-  //
-  // unsubscribeFromGamesList() {
-  //   if (this.gamesTimer !== null) clearInterval(this.gamesTimer)
-  // }
-  //
-  // subscribeToGame() {
-  //   const gameId = parseInt(getGameIdByLocation(store.getState() as IState))
-  //
-  //   if (!gameId || isNaN(gameId)) {
-  //     return
-  //   }
-  //
-  //   const update = () => {
-  //     gameStateService.getGameState(gameId).then((state) => {
-  //       store.dispatch(gameStateUpdate(state))
-  //     })
-  //   }
-  //
-  //   update()
-  //
-  //   this.gameTimer = setInterval(update, pollingTimeout)
-  // }
-  //
-  // unsubscribeFromGame() {
-  //   if (this.gameTimer !== null) clearInterval(this.gameTimer)
-  // }
 }
 
 export const sseService = new SseService()
