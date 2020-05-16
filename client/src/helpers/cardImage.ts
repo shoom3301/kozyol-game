4b5a2027e16958fac70ee640c321de32e2b9181c
a.e.kazachenko <a.e.kazachenko@tinkoff.ru>
2020-05-16 06:25:19 +0300

    Cards selfhosted

--- client/src/helpers/cardImage.ts
+++ client/src/helpers/cardImage.ts
@@ -1,5 +1,5 @@
 import { Card, SuitCharMap, ValueCharMap } from 'model/Card'
 
 export function cardImage([suit, value]: Card) {
-  return `https://richardschneider.github.io/cardsJS/cards/${ValueCharMap[value]}${SuitCharMap[suit]}.svg`
+  return `/cards/${ValueCharMap[value]}${SuitCharMap[suit]}.svg`
 }
